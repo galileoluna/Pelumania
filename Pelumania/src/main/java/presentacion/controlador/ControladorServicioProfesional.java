a13619c5dd0678288819b1e3d0aef1e94abc2a8e
nicolas.cirillo <nicolas.cirillo@NX-PC-010.necxus.bsas>
2019-10-02 16:20:28 -0300

    listo visualizacion de la relacion servicio profesional

--- Pelumania/src/main/java/presentacion/controlador/ControladorServicioProfesional.java
+++ Pelumania/src/main/java/presentacion/controlador/ControladorServicioProfesional.java
@@ -1,9 +1,14 @@
 package presentacion.controlador;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import dto.ProfesionalDTO;
 import modelo.Sistema;
+import persistencia.conexion.Conexion;
 import presentacion.vista.VentanaProfesional;
 import presentacion.vista.VentanaServicioProfecional;
 
@@ -12,6 +17,7 @@ public class ControladorServicioProfesional {
 	private Sistema sistema;
 	private String nombre;
 	private int idProfesional;
+	private List<String> servEnTabla; 
 	private VentanaServicioProfecional ventServProf;
 
 	private ControladorServicioProfesional(Sistema sistema) {
@@ -25,10 +31,35 @@ public class ControladorServicioProfesional {
 		}
 		INSTANCE.ventServProf.setNombreEmpl(nombre);
 		INSTANCE.idProfesional=idProf;
+		INSTANCE.servEnTabla=llenarLista(idProf);
+		INSTANCE.ventServProf.llenarTabla(INSTANCE.servEnTabla);
 		INSTANCE.ventServProf.show();
 		return INSTANCE;
 	}
 	
+
+	private static List<String> llenarLista(int id) {
+		PreparedStatement statement;
+		ResultSet resultSet;
+		Conexion conexion = Conexion.getConexion();
+		List<String> servicio = new ArrayList<String>();
+		try 
+		{
+			statement = conexion.getSQLConexion().prepareStatement("SELECT s.Nombre FROM servicioprofesional sp JOIN profesional p USING(idProfesional) JOIN servicio s USING (idServicio) WHERE p.IdProfesional=?");
+			statement.setInt(1, id);
+			resultSet = statement.executeQuery();
+			while (resultSet.next()){
+				servicio.add(resultSet.getString("s.Nombre"));
+			}
+			return servicio;
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		return null;
+			
+	}
 	
 
 }
