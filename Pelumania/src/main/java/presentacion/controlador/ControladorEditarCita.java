4e947a95efffe3e980509b283365b948d3dffc3a
MatiArriola <matiasarriola.luigi@gmail.com>
2019-10-18 13:21:02 -0300

    La tabla editarCita muestra lso servicios asociados a la cita

--- Pelumania/src/main/java/presentacion/controlador/ControladorEditarCita.java
+++ Pelumania/src/main/java/presentacion/controlador/ControladorEditarCita.java
@@ -1,6 +1,13 @@
 package presentacion.controlador;
 
+import java.math.BigDecimal;
+import java.time.LocalTime;
+import java.util.List;
+
 import dto.CitaDTO;
+import dto.ProfesionalDTO;
+import dto.ServicioDTO;
+import dto.ServicioTurnoDTO;
 import modelo.Sistema;
 import presentacion.vista.VentanaEditarCita;
 
@@ -14,6 +21,8 @@ public class ControladorEditarCita {
 	private ControladorEditarCita(Sistema sistema, CitaDTO citaAEditar) {
 		this.ventanaEditarCita = VentanaEditarCita.getInstance(citaAEditar);
 		this.sistema = sistema;
+		List<ServicioTurnoDTO> serviciosDeCita = this.sistema.getByIdCita(citaAEditar.getIdCita());
+		cargarServicios(serviciosDeCita);
 	}
 
 	public static ControladorEditarCita getInstance(Sistema sistema, CitaDTO citaAEditar) {
@@ -24,4 +33,27 @@ public class ControladorEditarCita {
 		return INSTANCE;
 	}
 
+	public void cargarServicios(List<ServicioTurnoDTO> serviciosDeCita) {
+		this.ventanaEditarCita.getCompCita().getModelServicios().setRowCount(0); //Para vaciar la tabla
+		this.ventanaEditarCita.getCompCita().getModelServicios().setColumnCount(0);
+		this.ventanaEditarCita.getCompCita().getModelServicios().setColumnIdentifiers(this.ventanaEditarCita.getCompCita().getNombreColumnas());
+
+		for (ServicioTurnoDTO st : serviciosDeCita)
+		{
+			
+			int idServ = st.getIdServicio();
+			int idProf = st.getIdProfesional();
+			
+			ServicioDTO serv = this.sistema.getServicioById(idServ);
+			ProfesionalDTO prof = this.sistema.getProfesionalById(idProf);
+			
+			String nombre = serv.getNombre();
+			BigDecimal precioLocal = serv.getPrecioLocal();
+			BigDecimal precioDolar = serv.getPrecioDolar();
+			LocalTime duracion = serv.getDuracion();
+			String nombreProf = prof.getNombre()+" "+prof.getApellido();
+			Object[] fila = {nombre, precioLocal, precioDolar, duracion, nombreProf};
+			this.ventanaEditarCita.getCompCita().getModelServicios().addRow(fila);
+		}
+	}
 }
