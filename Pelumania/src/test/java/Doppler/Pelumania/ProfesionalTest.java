f9ed859f3e36cca14f7e6426f36bb2e0ed0f445e
julian moreno <santy-moreno1@hotmail.com>
2019-10-22 01:33:37 +0000

    Update ProfesionalTest.java

--- Pelumania/src/test/java/Doppler/Pelumania/ProfesionalTest.java
+++ Pelumania/src/test/java/Doppler/Pelumania/ProfesionalTest.java
@@ -45,33 +45,35 @@ public class ProfesionalTest {
 	}
 	@Test
 	public void testValidarApellidoProfesional() {
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo=	new ProfesionalDTO(2, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
 	assertEquals(sistema.obtenerProfesional().get(0).getApellido(),"Mendoza");
 	}
 	@Test
 	public void testValidarSucursalOrigen() {
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo=	new ProfesionalDTO(3, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
 	assertEquals(sistema.obtenerProfesional().get(0).getIdSucursalOrigen(),1);
 	}
 	@Test
 	public void testValidarSucursalDestino() {
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo=	new ProfesionalDTO(5, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
 	assertEquals(sistema.obtenerProfesional().get(0).getIdSucursalTransferencia(),2);
 	}
 	@Test
 	public void testValidarEstado() {
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	int cantProfExistentes=sistema.obtenerProfesional().size();	
+	ProfesionalDTO nuevo=	new ProfesionalDTO(12, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
-	assertEquals(sistema.obtenerProfesional().get(0).getEstado(),"ACTIVO");
+	
+	assertEquals(sistema.obtenerProfesional().get(cantProfExistentes-1).getEstado(),"ACTIVO");
 	}
 	@Test
 	public void testEliminarProfesional() {
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo=	new ProfesionalDTO(11, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
-	ProfesionalDTO nuevo2=	new ProfesionalDTO(2, "Marcelo", "Polino" ,2, 3, "ACTIVO" );
+	ProfesionalDTO nuevo2=	new ProfesionalDTO(20, "Marcelo", "Polino" ,2, 3, "ACTIVO" );
 	sistema.agregarProfesional(nuevo2);
 	sistema.borrarProfesional(nuevo2);
 	assertEquals(sistema.obtenerProfesional().size(),1);
@@ -79,7 +81,7 @@ public class ProfesionalTest {
 	
 	@Test
 	public void testValidarEstadoBorrado() {
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo=	new ProfesionalDTO(8, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
 	sistema.borrarProfesional(nuevo);
 	assertEquals(sistema.obtenerProfesional().get(0).getEstado(),"Inactivo");
@@ -87,9 +89,9 @@ public class ProfesionalTest {
 	
 	@Test
 	public void testValidarUpdate() {
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo=	new ProfesionalDTO(9, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
-	ProfesionalDTO nuevo1=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 3, "ENFERMO" );
+	ProfesionalDTO nuevo1=	new ProfesionalDTO(17, "Flavio", "Mendoza" ,1, 3, "ENFERMO" );
 	sistema.actualizarProfesional(nuevo1);
 	assertEquals(sistema.obtenerProfesional().get(0).getEstado(),"ENFERMO");
 	assertEquals(sistema.obtenerProfesional().get(0).getIdSucursalTransferencia(),3);
@@ -97,8 +99,8 @@ public class ProfesionalTest {
 
 	@Test
 	public void testRedOne() {
-	ProfesionalDTO nuevo=new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
-	ProfesionalDTO nuevo1=new ProfesionalDTO(2, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo=new ProfesionalDTO(100, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo1=new ProfesionalDTO(200, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
 	sistema.agregarProfesional(nuevo1);
 	int id=nuevo1.getIdProfesional();
@@ -110,13 +112,13 @@ public class ProfesionalTest {
 	
 	@Test
 	public void testValidarUpdateporId() {
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo=	new ProfesionalDTO(101, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
 
 	}
 	@Test
 	public void test() {
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo=	new ProfesionalDTO(102, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
 	sistema.borrarProfesional(nuevo);
 	assertEquals(sistema.obtenerProfesional().get(0).getEstado(),"Inactivo");
@@ -124,7 +126,7 @@ public class ProfesionalTest {
 	
 	@Test
 	public void setEstado() {
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo=	new ProfesionalDTO(103, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
 	nuevo.setIdProfesional(2);
 	assertEquals(nuevo.getIdProfesional(),2);
@@ -132,14 +134,14 @@ public class ProfesionalTest {
 	
 	@Test
 	public void setNombre() {
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo=	new ProfesionalDTO(105, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
 	nuevo.setNombre("Franco");
 	assertEquals(nuevo.getNombre(),"Franco");
 	}
 	@Test
 	public void setApeliido() {
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo=	new ProfesionalDTO(106, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
 	nuevo.setApellido("Franco");
 	assertEquals(nuevo.getApellido(),"Franco");
@@ -148,7 +150,7 @@ public class ProfesionalTest {
 	
 	@Test
 	public void setESTADO() {
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo=	new ProfesionalDTO(107, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
 	nuevo.setEstado("Franco");
 	assertEquals(nuevo.getEstado(),"Franco");
@@ -156,7 +158,7 @@ public class ProfesionalTest {
 	
 	@Test
 	public void setIdSucursal() {
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo=	new ProfesionalDTO(110, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
 	nuevo.setIdSucursalTransferencia(3);
 
@@ -170,11 +172,12 @@ public class ProfesionalTest {
 	@Test
 	public void testValidarSize() {
 		
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
-	ProfesionalDTO nuevo1=	new ProfesionalDTO(2, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	int cantProfExistentes=sistema.obtenerProfesional().size();	
+	ProfesionalDTO nuevo=	new ProfesionalDTO(109, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo1=	new ProfesionalDTO(203, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
 	sistema.agregarProfesional(nuevo1);
-	assertEquals(sistema.obtenerProfesional().size(),2);
+	assertEquals(sistema.obtenerProfesional().size(),2+cantProfExistentes);
 	}
 	
 	
@@ -184,8 +187,8 @@ public class ProfesionalTest {
 	@Test(expected = SQLException.class)
 	public void errorMismoID() {
 		
-	ProfesionalDTO nuevo=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
-	ProfesionalDTO nuevo1=	new ProfesionalDTO(1, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo=	new ProfesionalDTO(400, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
+	ProfesionalDTO nuevo1=	new ProfesionalDTO(350, "Flavio", "Mendoza" ,1, 2, "ACTIVO" );
 	sistema.agregarProfesional(nuevo);
 	sistema.agregarProfesional(nuevo1);
 	}
