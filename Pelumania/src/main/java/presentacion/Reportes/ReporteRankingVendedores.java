959ba50a6cb835bf5c35068822d0fe415d75354c
julian moreno <santy-moreno1@hotmail.com>
2019-10-25 03:03:41 +0000

    Add new file

---
+++ Pelumania/src/main/java/presentacion/Reportes/ReporteRankingVendedores.java
@@ -0,0 +1,53 @@
+package presentacion.Reportes;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import dto.CitaDTO;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
+public class ReporteRankingVendedores {
+	private JasperReport reporte;
+	private JasperViewer reporteViewer;
+	private JasperPrint	reporteLleno;
+	private Logger log = Logger.getLogger(ReporteComprobante.class);
+	//Recibe la lista de personas para armar el reporte
+    public ReporteRankingVendedores(CitaDTO cita)
+    {
+    	
+    	List<CitaDTO> coleccion = new ArrayList<CitaDTO>();
+    	coleccion.add(cita);
+    	//Hardcodeado
+		Map<String, Object> parametersMap = new HashMap<String, Object>();
+		parametersMap.put("Fecha", new SimpleDateFormat("dd/MM/yyyy").format(new Date()));		
+    	try		{
+			this.reporte = (JasperReport) JRLoader.loadObjectFromFile("src" + File.separator + "main" + File.separator + "java" + File.separator +  "presentacion" + File.separator + "reportes" + File.separator + "ReporteRankingVendedores.jasper" );
+			this.reporteLleno = JasperFillManager.fillReport(this.reporte, parametersMap, 
+					new JRBeanCollectionDataSource(coleccion));
+    		log.info("Se cargó correctamente el reporte");
+		}
+		catch( JRException ex ) 
+		{
+			log.error("Ocurrió un error mientras se cargaba el archivo ReporteRankingVendedores.Jasper", ex);
+		}
+    }       
+    
+    public void mostrar()
+	{
+		this.reporteViewer = new JasperViewer(this.reporteLleno,false);
+		this.reporteViewer.setVisible(true);
+	}
+}
