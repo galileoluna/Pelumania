a8513fdf3911759d46e25bfb46e801a535675fb7
julian moreno <santy-moreno1@hotmail.com>
2019-10-27 16:05:50 +0000

    Se agregan las querys que faltaban

--- Pelumania/src/main/java/persistencia/dao/interfaz/MovimientoCajaDAO.java
+++ Pelumania/src/main/java/persistencia/dao/interfaz/MovimientoCajaDAO.java
@@ -9,13 +9,18 @@ public interface MovimientoCajaDAO {
 	public boolean insertEgreso(MovimientoCajaDTO movimiento_a_insertar);
 
 	public boolean delete(MovimientoCajaDTO movimiento_a_eliminar);
-	
-	public List<MovimientoCajaDTO> readAll();
-	
+		
 	public boolean deleteReal(MovimientoCajaDTO movimiento_a_eliminar);
 
 	public boolean insertIngresoServicioSinPromocion(MovimientoCajaDTO movimiento);
 
 	public boolean insertIngresoProducto(MovimientoCajaDTO movimiento_a_insertar);
 
+	public List<MovimientoCajaDTO> readAllEgresos();
+
+	public List<MovimientoCajaDTO> readAllIngresos();
+
+	public List<MovimientoCajaDTO> readDayEgresos(String fecha, String fecha2);
+	
+	public List<MovimientoCajaDTO> readDayIngresos(String fecha, String fecha2);
 }
