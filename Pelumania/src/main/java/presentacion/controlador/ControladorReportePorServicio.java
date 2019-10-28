992144010c32e57ba8d67fd566966786964a67ee
julian moreno <santy-moreno1@hotmail.com>
2019-10-28 00:02:15 +0000

    Update ControladorReportePorServicio.java

--- Pelumania/src/main/java/presentacion/controlador/ControladorReportePorServicio.java
+++ Pelumania/src/main/java/presentacion/controlador/ControladorReportePorServicio.java
@@ -1,9 +1,16 @@
 package presentacion.controlador;
 
+import java.awt.event.ActionEvent;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
+import dto.MovimientoCajaDTO;
 import dto.ServicioDTO;
 import modelo.Sistema;
+import presentacion.Reportes.ReportePorCliente;
+import presentacion.Reportes.ReportePorServicio;
 import presentacion.vista.VentanaReportePorServicio;
 
 public class ControladorReportePorServicio {
@@ -15,6 +22,9 @@ public class ControladorReportePorServicio {
 		this.ventanaReportes = VentanaReportePorServicio.getInstance();
 		this.sistema = sistema;
 		cargarServicios();
+		
+		this.ventanaReportes.getBtnGenerarReporte().addActionListener(l -> reportePorServicio(l));
+
 	}
 
 	public static ControladorReportePorServicio getInstance(Sistema sistema) {
@@ -30,5 +40,19 @@ public class ControladorReportePorServicio {
 		for (ServicioDTO servicio : servicios) {
 			this.ventanaReportes.getJcb_Servicio().addItem(servicio);
 		}
+	}
+	
+	private void reportePorServicio(ActionEvent l) {
+		SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+		String desde = formato.format(ventanaReportes.getJdc_Desde().getDate());
+		String hasta = formato.format(ventanaReportes.getJdc_Hasta().getDate());
+		//System.out.println(desde+" "+hasta);
+		
+		ArrayList<MovimientoCajaDTO>servicio=(ArrayList<MovimientoCajaDTO>) sistema.obtenerMovimientosCajaIngresosServicio(desde,hasta,this.ventanaReportes.getJcb_Servicio().getSelectedIndex());
+		System.out.println("hola");
+		System.out.println(servicio.get(0).getIdServicio());
+		
+		ReportePorServicio reportePorServicio = new ReportePorServicio(servicio);
+		reportePorServicio.mostrar();
 	}
 }
