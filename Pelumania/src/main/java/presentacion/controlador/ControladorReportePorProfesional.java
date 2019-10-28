5a6b449f6596e450679c85996cabe7db6c4b0257
julian moreno <santy-moreno1@hotmail.com>
2019-10-28 00:01:02 +0000

    Update ControladorReportePorProfesional.java

--- Pelumania/src/main/java/presentacion/controlador/ControladorReportePorProfesional.java
+++ Pelumania/src/main/java/presentacion/controlador/ControladorReportePorProfesional.java
@@ -1,9 +1,16 @@
 package presentacion.controlador;
 
+import java.awt.event.ActionEvent;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
+import dto.MovimientoCajaDTO;
 import dto.ProfesionalDTO;
 import modelo.Sistema;
+import presentacion.Reportes.ReportePorCliente;
+import presentacion.Reportes.ReportePorProfesional;
 import presentacion.vista.VentanaReportePorProfesional;
 
 public class ControladorReportePorProfesional {
@@ -15,6 +22,9 @@ public class ControladorReportePorProfesional {
 		this.ventanaReportes = VentanaReportePorProfesional.getInstance();
 		this.sistema = sistema;
 		cargarProfesional();
+		
+		this.ventanaReportes.getBtnGenerarReporte().addActionListener(l -> reportePorProfesional(l));
+
 	}
 
 	public static ControladorReportePorProfesional getInstance(Sistema sistema) {
@@ -30,5 +40,19 @@ public class ControladorReportePorProfesional {
 		for (ProfesionalDTO profesional : profesionales) {
 			this.ventanaReportes.getJcb_Profesional().addItem(profesional);
 		}
+	}
+	
+	private void reportePorProfesional(ActionEvent l) {
+		SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+		String desde = formato.format(ventanaReportes.getJdc_Desde().getDate());
+		String hasta = formato.format(ventanaReportes.getJdc_Hasta().getDate());
+		//System.out.println(desde+" "+hasta);
+		
+		ArrayList<MovimientoCajaDTO>profesional=(ArrayList<MovimientoCajaDTO>) sistema.obtenerMovimientosCajaIngresosProfesional(desde,hasta,this.ventanaReportes.getJcb_Profesional().getSelectedIndex());
+		System.out.println("hola");
+		System.out.println(profesional.get(0).getIdProfesional());
+		
+		ReportePorProfesional reportePorProf = new ReportePorProfesional(profesional);
+		reportePorProf.mostrar();
 	}
 }
