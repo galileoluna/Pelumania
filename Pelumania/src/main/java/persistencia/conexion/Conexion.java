2707abe492f72c879330545e6849a91f10df3c45
nicolas.cirillo <nicolas.cirillo@NX-PC-010.necxus.bsas>
2019-10-29 16:12:22 -0300

    pusheo con conexion a la bdd local

--- Pelumania/src/main/java/persistencia/conexion/Conexion.java
+++ Pelumania/src/main/java/persistencia/conexion/Conexion.java
@@ -23,8 +23,8 @@ public class Conexion {
 				
 				// *************************************   Para BD REMOTA  *********************************************
 				// *****************************************************************************************************
-//				Class.forName("com.mysql.cj.jdbc.Driver"); 
-//				this.connection = DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net:3306/sql10309995?serverTimezone=UTC","sql10309995","mS2tbDhSF2");
+				//Class.forName("com.mysql.cj.jdbc.Driver"); 
+				//this.connection = DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net:3306/sql10309995?serverTimezone=UTC","sql10309995","mS2tbDhSF2");
 				// *****************************************************************************************************
 				
 //				this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/PelumaniaTest?serverTimezone=UTC","root","@Root123");
