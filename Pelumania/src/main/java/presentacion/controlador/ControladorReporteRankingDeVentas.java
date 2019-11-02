299b5c0836707da6d270c08482cdceed81d238fd
julian moreno <santy-moreno1@hotmail.com>
2019-11-02 16:46:18 +0000

    Update ControladorReporteRankingDeVentas.java

--- Pelumania/src/main/java/presentacion/controlador/ControladorReporteRankingDeVentas.java
+++ Pelumania/src/main/java/presentacion/controlador/ControladorReporteRankingDeVentas.java
@@ -1,6 +1,13 @@
 package presentacion.controlador;
 
+import java.awt.event.ActionEvent;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import dto.MovimientoCajaDTO;
 import modelo.Sistema;
+import presentacion.Reportes.ReporteDeCajaPorSucursal;
 import presentacion.vista.VentanaReporteRankingVentas;
 
 public class ControladorReporteRankingDeVentas {
@@ -11,6 +18,9 @@ public class ControladorReporteRankingDeVentas {
 	private ControladorReporteRankingDeVentas(Sistema sistema) {
 		this.ventanaReportes = VentanaReporteRankingVentas.getInstance();
 		this.sistema = sistema;
+
+		this.ventanaReportes.getBtnGenerarReporte().addActionListener(l -> reporteVentas(l));
+
 	}
 
 	public static ControladorReporteRankingDeVentas getInstance(Sistema sistema) {
@@ -19,4 +29,21 @@ public class ControladorReporteRankingDeVentas {
 		}
 		return INSTANCE;
 	}
+	
+	private void reporteVentas(ActionEvent l) {
+		SimpleDateFormat formato = new SimpleDateFormat("YYYY-MM-dd", Locale.getDefault());
+		String desde = formato.format(ventanaReportes.getJdc_Desde().getDate());
+		String hasta = formato.format(ventanaReportes.getJdc_Hasta().getDate());
+			
+		String desdeParaReporte=desde+" 00:00:01";
+		String hastaParaReporte=hasta+" 11:59:59";
+			
+		ArrayList<MovimientoCajaDTO>caja=(ArrayList<MovimientoCajaDTO>) sistema.obtenerMovimientoCajaRanking(desdeParaReporte,hastaParaReporte);
+
+		ReporteDeCajaPorSucursal reporteDeCajaSucursal = new ReporteDeCajaPorSucursal(caja,desde,hasta);
+		reporteDeCajaSucursal.mostrar();
+			
+	}
+	
 }
+
