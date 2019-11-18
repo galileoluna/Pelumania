aaea761815306cf39116fa9780faa8ab9746866a
julian moreno <santy-moreno1@hotmail.com>
2019-11-18 20:18:44 +0000

    Update ClienteDTOtest.java

--- Pelumania/src/test/java/Doppler/Pelumania/ClienteDTOtest.java
+++ Pelumania/src/test/java/Doppler/Pelumania/ClienteDTOtest.java
@@ -19,62 +19,83 @@ class ClienteDTOtest {
     @Test
 	public void testValidarGetID() {
 		BigDecimal deuda=new BigDecimal(5);
-		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
 		
 		assertEquals(cliente.getIdCliente(),1);
 	}
 	@Test
 	public void testValidarGetApellido() {
 		BigDecimal deuda=new BigDecimal(5);
-		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
 		
 		assertEquals(cliente.getApellido(),"apellido");
 	}
 	@Test
 	public void testValidarGetNombre() {
 		BigDecimal deuda=new BigDecimal(5);
-		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
 		
 		assertEquals(cliente.getNombre(),"juan");
 	}@Test
 	public void testValidarGetTelefono() {
 		BigDecimal deuda=new BigDecimal(5);
-		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
 		
 		assertEquals(cliente.getTelefono(),"112345");
 	}
 	@Test
 	public void testValidarGetMail() {
 		BigDecimal deuda=new BigDecimal(5);
-		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
 		
 		assertEquals(cliente.getMail(),"asd@sddd.com");
 	}
 	@Test
 	public void testValidarGetPuntos() {
 		BigDecimal deuda=new BigDecimal(5);
-		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
 		
 		assertEquals(cliente.getPuntos(),1);
 	}
 	@Test
 	public void testValidarGetEstado() {
 		BigDecimal deuda=new BigDecimal(5);
-		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
 		
 		assertEquals(cliente.getEstadoCliente(),"activo");
 	}
 	@Test
-	public void testValidarGetDeuda() {
+	public void testValidarGetDeudaDolar() {
 		BigDecimal deuda=new BigDecimal(5);
-		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
 		
-		assertEquals(cliente.getDeuda().compareTo(deuda),0);
+		assertEquals(cliente.getDeudaDolar().compareTo(deuda),0);
+	}
+	@Test
+	public void testValidarGetDeudaPesos() {
+		BigDecimal deuda=new BigDecimal(5);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
+		
+		assertEquals(cliente.getDeudaPesos().compareTo(deuda),0);
+	}
+	@Test
+	public void testValidarSetDeudaDolar() {
+		BigDecimal deuda=new BigDecimal(5);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
+		cliente.setDeudaDolar(new BigDecimal(4));
+		assertEquals(cliente.getDeudaDolar().compareTo(new BigDecimal(4)),0);
+	}
+	@Test
+	public void testValidarSetDeudaPesos() {
+		BigDecimal deuda=new BigDecimal(5);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
+		cliente.setDeudaPesos(new BigDecimal(4));
+		assertEquals(cliente.getDeudaPesos().compareTo(new BigDecimal(4)),0);
 	}
 	@Test
 	public void testValidarSetID() {
 		BigDecimal deuda=new BigDecimal(5);
-		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
 		
 		cliente.setIdCliente(5);
 		assertEquals(cliente.getIdCliente(),5);
@@ -82,7 +103,7 @@ class ClienteDTOtest {
 	@Test
 	public void testValidarSetNombre() {
 		BigDecimal deuda=new BigDecimal(5);
-		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
 		
 		cliente.setNombre("pedro");
 		assertEquals(cliente.getNombre(),"pedro");
@@ -90,7 +111,7 @@ class ClienteDTOtest {
 	@Test
 	public void testValidarSetApellido() {
 		BigDecimal deuda=new BigDecimal(5);
-		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
 		
 		cliente.setApellido("moreno");
 		assertEquals(cliente.getApellido(),"moreno");
@@ -98,7 +119,7 @@ class ClienteDTOtest {
 	@Test
 	public void testValidarSetTelefono() {
 		BigDecimal deuda=new BigDecimal(5);
-		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
 		
 		cliente.setTelefono("1137896016");
 		assertEquals(cliente.getTelefono(),"1137896016");
@@ -106,15 +127,15 @@ class ClienteDTOtest {
 	@Test
 	public void testValidarSetMail() {
 		BigDecimal deuda=new BigDecimal(5);
-		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
 		
-		cliente.setTelefono("santy@gmail.com");
-		assertEquals(cliente.getTelefono(),"santy@gmail.com");
+		cliente.setMail("santy@gmail.com");
+		assertEquals(cliente.getMail(),"santy@gmail.com");
 	}
 	@Test
 	public void testValidarSetEstado() {
 		BigDecimal deuda=new BigDecimal(5);
-		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
 		
 		cliente.setEstadoCliente("inactivo");
 		assertEquals(cliente.getEstadoCliente(),"inactivo");
@@ -122,9 +143,16 @@ class ClienteDTOtest {
 	@Test
 	public void testValidarSetPuntos() {
 		BigDecimal deuda=new BigDecimal(5);
-		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
 		
 		cliente.setPuntos(15);
 		assertEquals(cliente.getPuntos(),15);
+	}
+	@Test
+	public void testValidarToString() {
+		BigDecimal deuda=new BigDecimal(5);
+		cliente=new ClienteDTO(1,"juan","apellido","112345","asd@sddd.com",1,"activo",deuda,deuda);
+		
+		assertTrue(cliente.toString().equals("juan apellido"));
 	}
 }
